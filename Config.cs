4d4ace94b684b11179ed049526225559d811449d
Igor Machado <51010839+Sfrev@users.noreply.github.com>
2024-08-19 10:52:58 -0300

    Api funcionando e melhorias na leitura do xml

---
+++ Config.cs
@@ -0,0 +1,17 @@
+﻿namespace desafio_bt
+{
+    public class Config(string token, string destinationEmail, string host, int port, string username, string password)
+    {
+        private readonly string token = token;
+        private readonly string destinationEmail = destinationEmail;
+        private readonly string host = host;
+        private readonly int port = port;
+        private readonly string username = username;
+        private readonly string password = password;
+
+        public String GetToken()
+        {
+            return token;
+        }
+    }
+}
